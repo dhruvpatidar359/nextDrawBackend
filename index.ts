67d5471c739e72cc007a83c2ed35d647b21722aa
Dhruv Patidar <103873587+dhruvpatidar359@users.noreply.github.com>
2024-02-24 22:23:48 +0530

    removed consoles

--- index.ts
+++ index.ts
@@ -118,8 +118,7 @@ io.on('connection', (socket) => {
 
         if (!rooms[roomId]) {
             rooms[roomId] = {};
-            console.log(elements);
-            console.log("--------------------");
+          
             for (let i = 0; i < elements.length; i++) {
                 const element = elements[i];
                 // as undo redo not supported right now
@@ -166,8 +165,7 @@ io.on('connection', (socket) => {
                 socket.join(roomId);
                 socket.to(roomId).emit('user joined', socket.id);
                 if (rooms[roomId]) {
-                    console.log("sending the data to the joined user");
-                    console.log(rooms[roomId]);
+                   
                     for (let key in rooms[roomId]) {
 
                         const tempNewArray = rooms[roomId][key];
